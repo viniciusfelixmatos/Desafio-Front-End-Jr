49d70fbe1e1b9bf91e50efbec23a67535bb68cd2
Vinicius Matos <163915966+viniciusfelixmatos@users.noreply.github.com>
2026-02-23 11:57:03 -0300

    fix: Correção no nome do import para a Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import Header from "./components/header/header"
+import Header from './components/Header/Header'
 import Main from "./components/main/Main"
 import Footer from "./components/footer/Footer"
 import './App.css'
