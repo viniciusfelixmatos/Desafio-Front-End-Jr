bda7c76dac01e5fcf2c6094e62916dde30fca75b
Vinicius Matos <163915966+viniciusfelixmatos@users.noreply.github.com>
2026-02-25 09:02:10 -0300

    fix: Teste sobre o img não encontrado na vercel

--- src/components/main components/midia/Midia.jsx
+++ src/components/main components/midia/Midia.jsx
@@ -143,7 +143,7 @@ function Midia({videos, posters, backgrounds}) {
                             breakpoints={{
                                 0: { slidesPerView: 1 },
                                 576: { slidesPerView: 2 },
-                                992: { slidesPerView: 3 },
+                                992: { slidesPerView: 2 },
                                 1200: { slidesPerView: 3 }
                             }}
                         >
